6600d0f1aca92406fdd775dd34a70f075e4ea689
blakk-ebuka <killduke101@gmail.com>
2022-12-01 11:11:17 -0800

    Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from add_0 import add
-    a = 1
-    b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    m = 1
+    n = 2
+    print("{:d} + {:d} = {:d}".format(m, n, add(m, n)))
